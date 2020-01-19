377a33ad5fd47c7ba4a3e6fdba3ffb1fcc83a139
Henrique <henrique.campaner@live.com>
2020-01-19 14:41:03 +0000

    route test

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ require('dotenv/config');
 module.exports = {
   dialect: 'postgres',
   host: process.env.DB_HOST,
-  port: 5433,
+  port: 5432,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
